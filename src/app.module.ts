7f7325960465a8fc0730f47ed6ad4a20a8859e02
Pham Duy Luan <devilstorm.luan@gmail.com>
2022-03-29 02:26:59 +0700

    feat(article): create new module article Create module, resolver and serivce to project Add: #5

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { ArticleModule } from './article/article.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { UserModule } from './user/user.module';
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
+    ArticleModule,
   ],
   controllers: [AppController],
   providers: [AppService],
