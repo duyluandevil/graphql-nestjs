deab036017f6a51d01bf1d65daa5807f20d005c5
Pham Duy Luan <devilstorm.luan@gmail.com>
2022-03-29 18:10:37 +0700

    feat(article): create listArticle filter category

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -2,11 +2,13 @@
 import { Injectable } from '@nestjs/common';
 import article from 'src/data/article';
 import { Article } from './article.schema';
+import { ArticleRCategoryService } from '../article_r_category/article_r_category.service';
+import articleRCategory from 'src/data/article.r.category';
 
 @Injectable()
 export class ArticleService {
   article: Partial<Article>[];
-  constructor() {
+  constructor(private articleRCategoryService: ArticleRCategoryService) {
     this.article = article;
   }
 
@@ -20,7 +22,29 @@ export class ArticleService {
     // console.log(user.find(e => e.id.toString() == id))
   }
 
-  async filterUser(userId: string){
-    return article.filter(e=>e.userid.toString() === userId);
+  async filterUser(userId: string) {
+    return article.filter((e) => e.userid.toString() === userId);
   }
+
+  async filterCategory(categoryId: string) {
+    // articleRCategory.filter(e => e.categoryId === categoryId.toString())
+
+    //get value from detail 
+    let arrayMtach = articleRCategory.filter(e => e.categoryId === categoryId.toString())
+
+    let arrayArticleId = []; // array has id of article
+    arrayMtach.forEach(e => arrayArticleId.push(e.articleId))
+
+    let arrayResultEnd = []; // create array for return articles
+    arrayArticleId.forEach(e=> 
+      arrayResultEnd.push(article.find(ele => ele.id.toString() === e))
+      )
+    
+    return arrayResultEnd;
+
+
+    
+  }
+
+
 }
